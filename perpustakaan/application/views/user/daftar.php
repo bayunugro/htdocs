fe047941cdb0f19ab99d73bac7a7bb9989c0fe61
Bayu Nugroho <bayu.nugro@gmail.com>
2018-07-23 12:02:26 +0700

    1

--- perpustakaan/application/views/user/daftar.php
+++ perpustakaan/application/views/user/daftar.php
@@ -70,6 +70,9 @@
         </form>
       </div>
       </div>
+  <!-- Footer -->
+  <?php include_once "footer.php"; ?>
+  <!-- Footer -->
     <script src="<?php echo base_url();?>/assets/js/jquery-3.3.1.min.js"></script>
     <script src="<?php echo base_url();?>/assets/js/bootstrap.min.js"></script>
     <script src="<?php echo base_url();?>/assets/js/popper.min.js"></script>
